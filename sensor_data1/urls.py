35f9887b1bc46c7ed51f57f65f95965632560510
AndrewTurinawe <turinskandrew69@gmail.com>
2024-07-16 16:05:27 +0300

    some-changes

--- sensor_data1/urls.py
+++ sensor_data1/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('data1/average/', average_sensor_data_view, name='average-sensor-data'),
     path('data1/send-sms/', send_sms_view, name='send-sms'),
     path('data1/report/', co2_report_view, name='co2-report'),
+    path('data1/first-70/', fetch_first_70_values, name='first-70-sensor-data'),
+    path('data1/assign-daywise/', assign_daywise_values, name='assign-daywise-values'),
+     path('data1/fetch-next-70/', fetch_next_70_values, name='fetch-next-70-values'),
+    path('data1/assign-daywise2/', assign_daywise_values2, name='assign-daywise-values2'),
 ]
