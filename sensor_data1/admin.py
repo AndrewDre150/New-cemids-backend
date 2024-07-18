92dceb346cfecb382172c1d13c529104f916dced
AndrewTurinawe <turinskandrew69@gmail.com>
2024-07-18 15:10:58 +0300

    line-changes

--- sensor_data1/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SensorData1
-
-# Register your models here.
-admin.site.register(SensorData1)
--- sensor_data1/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SensorData1Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sensor_data1'
